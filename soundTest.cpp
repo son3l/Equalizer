9950fb51036e3b80992b5953af72b3d3f28d2113
bulavko <bulavko@KGT-ASUPP-17>
2025-01-30 16:26:50 +0700

    разделены устройства захвата и рендеринга звука (захват с vac, рендер на наушники), переход на fft на float

--- soundTest.cpp
+++ soundTest.cpp
@@ -8,41 +8,230 @@
 #include <vector>
 #include <comdef.h>
 #include <cmath>
+#include <fftw3.h>
+#include <algorithm>
+#include <mfapi.h>
+#include <mfidl.h>
+#include <mfobjects.h>
+#include <mfplay.h>
+#include <propkey.h> 
+#include <Functiondiscoverykeys_devpkey.h>
+const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
+const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
 
-
-void ProcessAudioData(std::vector<BYTE>& processedData, BYTE* buffer, UINT32 numFrames, UINT32 numChannels,UINT32 sampleRate, float gain) {
-    // Преобразуем буфер в float* для обработки
-    float* floatBuffer = reinterpret_cast<float*>(buffer);
-
-    // Для каждого сэмпла в буфере
-    for (UINT32 i = 0; i < numFrames * numChannels/2; ++i) {
-        // Применяем усиление
-        float audio;
-        float freq = (float)i * sampleRate / (numFrames * numChannels);
+void ProcessAudioData(BYTE* processedData, BYTE* buffer, UINT32 numFrames, UINT32 numChannels,UINT32 sampleRate, float gain) {
+    std::vector<float> processingData;
+    processingData.resize(numFrames);
+    short* pcmData = reinterpret_cast<INT16*>(buffer);
+    for (UINT32 i = 0; i < numFrames; ++i) {
+        processingData[i] = pcmData[i] / 32768.0f; // Преобразуем 16-битные значения в float
+    }
+    int N = processingData.size();
+    fftwf_complex* fftData = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex) * N);
+    fftwf_plan planForward = fftwf_plan_dft_r2c_1d(N, processingData.data(), fftData, FFTW_ESTIMATE);
+    //todo пофиксить баг
+    //// Применяем FFT
+    fftwf_execute(planForward);
+    /*for (int i = 0; i < N / 2 + 1; ++i) {
+        float freq = (float)i * sampleRate / N;
         if (freq < 1000) {
-            audio = floatBuffer[i] * gain;
+            fftData[i][0] *= gain;  // Усиливаем реальную часть
+            fftData[i][1] *= gain;  // Усиливаем мнимую часть
         }
-        else
-        {
-            audio = floatBuffer[i];
-        }
-        // Копируем результат в processedData
-        // Преобразуем результат обратно в байты и добавляем в processedData
-        processedData.push_back(reinterpret_cast<BYTE*>(&audio)[0]);
-        processedData.push_back(reinterpret_cast<BYTE*>(&audio)[1]);
-        processedData.push_back(reinterpret_cast<BYTE*>(&audio)[2]);
-        processedData.push_back(reinterpret_cast<BYTE*>(&audio)[3]);
+    }*/
+    fftwf_plan planBackward = fftwf_plan_dft_c2r_1d(N, fftData, processingData.data(), FFTW_ESTIMATE);
+    fftwf_execute(planBackward);
+    fftwf_destroy_plan(planForward);
+    fftwf_destroy_plan(planBackward);
+    fftwf_free(fftData);
+    for (int i = 0; i < N; ++i) {
+        processingData[i] /= N;
+    }
+    short* processedPcmData = reinterpret_cast<short*>(processedData);
+    // Преобразуем данные обратно в 16-битные целые числа, с учетом нормализации в диапазоне [-1, 1]
+    for (size_t i = 0; i < processingData.size(); ++i) {
+        processedPcmData[i] = static_cast<short>(std::clamp(processingData[i]* 32767.0f, -32768.0f, 32767.0f));
     }
 }
-
-
-IMMDevice* getAudioEndpointDevice()
+HRESULT GetVACDevice(IMMDevice** ppDevice)
 {
+    HRESULT hr = S_OK;
+    IMMDeviceEnumerator* pEnumerator = nullptr;
+    IMMDeviceCollection* pCollection = nullptr;
+    UINT deviceCount = 0;
     IMMDevice* pDevice = nullptr;
+    LPWSTR deviceName;
+
+    // Инициализация COM
+    hr = CoInitialize(nullptr);
+    if (FAILED(hr)) {
+        std::cout << "COM initialization failed!" << std::endl;
+        return hr;
+    }
+
+    // Создание объекта для перечисления устройств
+    hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_ALL, IID_PPV_ARGS(&pEnumerator));
+    if (FAILED(hr)) {
+        std::cout << "Failed to create IMMDeviceEnumerator!" << std::endl;
+        CoUninitialize();
+        return hr;
+    }
+
+    // Получение списка всех аудиоустройств
+    hr = pEnumerator->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE, &pCollection);
+    if (FAILED(hr)) {
+        std::cout << "Failed to enumerate audio devices!" << std::endl;
+        pEnumerator->Release();
+        CoUninitialize();
+        return hr;
+    }
+
+    // Получаем количество устройств
+    hr = pCollection->GetCount(&deviceCount);
+    if (FAILED(hr)) {
+        std::cout << "Failed to get device count!" << std::endl;
+        pCollection->Release();
+        pEnumerator->Release();
+        CoUninitialize();
+        return hr;
+    }
+
+    // Перебор устройств
+    for (UINT i = 0; i < deviceCount; i++) {
+        hr = pCollection->Item(i, &pDevice);
+        if (FAILED(hr)) {
+            std::cout << "Failed to get device!" << std::endl;
+            continue;
+        }
+
+        // Получение имени устройства
+        IPropertyStore* pPropertyStore = nullptr;
+        PROPVARIANT prop;
+        PropVariantInit(&prop);
+        hr = pDevice->OpenPropertyStore(STGM_READ, &pPropertyStore);
+        if (FAILED(hr)) {
+            pDevice->Release();
+            continue;
+        }
+
+        hr = pPropertyStore->GetValue(PKEY_Device_FriendlyName, &prop);
+        if (FAILED(hr) || prop.vt == VT_EMPTY) {
+            pPropertyStore->Release();
+            pDevice->Release();
+            continue;
+        }
+        // Проверяем имя устройства (например, "Virtual Audio Cable")
+        if (wcsstr(prop.pwszVal, L"Virtual Audio Cable") != nullptr) {
+            *ppDevice = pDevice;
+            pPropertyStore->Release();
+            pEnumerator->Release();
+            pCollection->Release();
+            CoUninitialize();
+            return S_OK; // Найдено виртуальное аудиоустройство VAC
+        }
+
+        pPropertyStore->Release();
+        pDevice->Release();
+    }
+
+    // Если устройство не найдено
+    pEnumerator->Release();
+    pCollection->Release();
+    CoUninitialize();
+    return E_FAIL; // Устройство не найдено
+}
+HRESULT GetRenderDevice(IMMDevice** ppDevice)
+{
+    HRESULT hr = S_OK;
+    IMMDeviceEnumerator* pEnumerator = nullptr;
+    IMMDeviceCollection* pCollection = nullptr;
+    UINT deviceCount = 0;
+    IMMDevice* pDevice = nullptr;
+
+    // Инициализация COM
+    hr = CoInitialize(nullptr);
+    if (FAILED(hr)) {
+        std::cout << "COM initialization failed!" << std::endl;
+        return hr;
+    }
+
+    // Создание объекта для перечисления устройств
+    hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_ALL, IID_PPV_ARGS(&pEnumerator));
+    if (FAILED(hr)) {
+        std::cout << "Failed to create IMMDeviceEnumerator!" << std::endl;
+        CoUninitialize();
+        return hr;
+    }
+
+    // Получение списка всех аудиоустройств
+    hr = pEnumerator->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE, &pCollection);
+    if (FAILED(hr)) {
+        std::cout << "Failed to enumerate audio devices!" << std::endl;
+        pEnumerator->Release();
+        CoUninitialize();
+        return hr;
+    }
+
+    // Получаем количество устройств
+    hr = pCollection->GetCount(&deviceCount);
+    if (FAILED(hr)) {
+        std::cout << "Failed to get device count!" << std::endl;
+        pCollection->Release();
+        pEnumerator->Release();
+        CoUninitialize();
+        return hr;
+    }
+
+    // Перебор устройств
+    for (UINT i = 0; i < deviceCount; i++) {
+        hr = pCollection->Item(i, &pDevice);
+        if (FAILED(hr)) {
+            std::cout << "Failed to get device!" << std::endl;
+            continue;
+        }
+
+        // Получение имени устройства
+        IPropertyStore* pPropertyStore = nullptr;
+        PROPVARIANT prop;
+        PropVariantInit(&prop);
+        hr = pDevice->OpenPropertyStore(STGM_READ, &pPropertyStore);
+        if (FAILED(hr)) {
+            pDevice->Release();
+            continue;
+        }
+
+        hr = pPropertyStore->GetValue(PKEY_Device_FriendlyName, &prop);
+        if (FAILED(hr) || prop.vt == VT_EMPTY) {
+            pPropertyStore->Release();
+            pDevice->Release();
+            continue;
+        }
+        // Проверяем имя устройства (например, "Virtual Audio Cable")
+        if (wcsstr(prop.pwszVal, L"AirPods Pro") != nullptr) {
+            *ppDevice = pDevice;
+            pPropertyStore->Release();
+            pEnumerator->Release();
+            pCollection->Release();
+            CoUninitialize();
+            return S_OK; // Найдено виртуальное аудиоустройство VAC
+        }
+
+        pPropertyStore->Release();
+        pDevice->Release();
+    }
+
+    // Если устройство не найдено
+    pEnumerator->Release();
+    pCollection->Release();
+    CoUninitialize();
+    return E_FAIL; // Устройство не найдено
+}
+void GetDefaultAudioEndpointDevice(IMMDevice** pDevice)
+{
     HRESULT hr = CoInitialize(nullptr);
     if (FAILED(hr)) {
         std::cerr << "CoInitialize failed!" << std::endl;
-        return nullptr;
+        return ;
     }
 
     // Получаем интерфейс устройства вывода
@@ -51,73 +240,35 @@ IMMDevice* getAudioEndpointDevice()
     if (FAILED(hr)) {
         std::cerr << "Failed to create device enumerator!" << std::endl;
         CoUninitialize();
-        return nullptr;
+        return ;
     }
 
     // Получаем стандартное устройство вывода (динамики или наушники)
-    hr = pDeviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &pDevice);
+    hr = pDeviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, pDevice);
     if (FAILED(hr)) {
         std::cerr << "Failed to get default audio endpoint!" << std::endl;
         pDeviceEnumerator->Release();
         CoUninitialize();
-        return nullptr;
+        return ;
     }
     pDeviceEnumerator->Release();
-    return pDevice;
 }
 
 
 int main()
 {
-    //#pragma region  volume scalar
-    //    IMMDevice* pDevice = nullptr;
-    //    getAudioEndpointDevice(pDevice);
-    //    HRESULT hr = 0;
-    //    // Получаем интерфейс IAudioEndpointVolume для изменения громкости
-    //    IAudioEndpointVolume* pAudioEndpointVolume = nullptr;
-    //    hr = pDevice->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_ALL, nullptr, reinterpret_cast<void**>(&pAudioEndpointVolume));
-    //    if (FAILED(hr)) {
-    //        std::cerr << "Failed to activate audio endpoint volume interface!" << std::endl;
-    //        pDevice->Release();
-    //        CoUninitialize();
-    //        return -1;
-    //    }
-    //
-    //    // Получаем текущий уровень громкости
-    //    float currentVolume = 0.0f;
-    //    hr = pAudioEndpointVolume->GetMasterVolumeLevelScalar(&currentVolume);
-    //    if (FAILED(hr)) {
-    //        std::cerr << "Failed to get current volume!" << std::endl;
-    //    }
-    //    else {
-    //        std::cout << "Current volume: " << currentVolume * 100 << "%" << std::endl;
-    //    }
-    //
-    //    // Устанавливаем новый уровень громкости (например, 50%)
-    //    float newVolume = 0.5f;  // от 0.0f (тишина) до 1.0f (максимальная громкость)
-    //    hr = pAudioEndpointVolume->SetMasterVolumeLevelScalar(newVolume, nullptr);
-    //    if (FAILED(hr)) {
-    //        std::cerr << "Failed to set new volume!" << std::endl;
-    //    }
-    //    else {
-    //        std::cout << "Volume set to: " << newVolume * 100 << "%" << std::endl;
-    //    }
-    //
-    //    // Освобождаем ресурсы
-    //    pAudioEndpointVolume->Release();
-    //    pDevice->Release();
-    //
-    //    // Завершаем работу COM
-    //    CoUninitialize();
-    //    return 0;
-    //#pragma endregion
-    IMMDevice* pDevice = getAudioEndpointDevice();
+    IMMDevice* pDevice = nullptr;
+    //GetDefaultAudioEndpointDevice(&pDevice);
+    GetRenderDevice(&pDevice);
+    IMMDevice* pVACDevice = nullptr;
+    GetVACDevice(&pVACDevice);
+
 #pragma region capture client
     IAudioClient* pAudioClient = nullptr;
-    HRESULT hr = pDevice->Activate(__uuidof(IAudioClient), CLSCTX_ALL, nullptr, reinterpret_cast<void**>(&pAudioClient));
+    HRESULT hr = pVACDevice->Activate(__uuidof(IAudioClient), CLSCTX_ALL, nullptr, reinterpret_cast<void**>(&pAudioClient));
     if (FAILED(hr)) {
         std::cerr << "Failed to activate audio client!" << std::endl;
-        pDevice->Release();
+        pVACDevice->Release();
         CoUninitialize();
         return 0;
     }
@@ -126,7 +277,7 @@ int main()
     if (FAILED(hr)) {
         std::cerr << "Failed to get mix format!" << std::endl;
         pAudioClient->Release();
-        pDevice->Release();
+        pVACDevice->Release();
         CoUninitialize();
         return 0;
     }
@@ -134,7 +285,7 @@ int main()
     if (FAILED(hr)) {
         std::cerr << "Failed to initialize audio client!" << std::endl;
         pAudioClient->Release();
-        pDevice->Release();
+        pVACDevice->Release();
         CoUninitialize();
         return 0;
     }
@@ -143,7 +294,7 @@ int main()
     if (FAILED(hr)) {
         std::cerr << "Failed to get capture client!" << std::endl;
         pAudioClient->Release();
-        pDevice->Release();
+        pVACDevice->Release();
         CoUninitialize();
         return 0;
     }
@@ -207,7 +358,7 @@ int main()
     UINT32 numFramesAvailable;
     DWORD flags;
 
-    std::vector<BYTE> processedData;
+    //std::vector<BYTE> processedData;
     //for (int i = 0; i < 1000; ++i) {  // Пример 100 итераций
     while (true)
     {
@@ -216,18 +367,22 @@ int main()
             std::cerr << "Failed to get buffer!" << std::endl;
             break;
         }
-
-        // Применение обработки (например, усиление)
-        ProcessAudioData(processedData, pData, numFramesAvailable, pFormat->nChannels,pFormat->nSamplesPerSec,2.5f);  // Увеличение громкости на 1.5x
-        // Отправляем обработанные данные на вывод
-        BYTE* pRenderData = nullptr;
-        hr = pAudioRender->GetBuffer(numFramesAvailable, &pRenderData);
-        if (FAILED(hr)) {
-            std::cerr << "Failed to get render buffer!" << std::endl;
-            break;
-        }
-        // Копируем обработанные данные в буфер рендеринга
-        memcpy(pRenderData, processedData.data(), processedData.size());
+        if (pData != nullptr)
+        {
+            // Применение обработки (например, усиление)
+            BYTE* pRenderData = nullptr;
+            hr = pAudioRender->GetBuffer(numFramesAvailable, &pRenderData);
+            if (SUCCEEDED(hr))
+            {
+                ProcessAudioData(pRenderData, pData, numFramesAvailable, pFormat->nChannels, pFormat->nSamplesPerSec, 1.5f);  // Увеличение громкости на 1.5x
+                hr = pAudioRender->ReleaseBuffer(numFramesAvailable, 0);
+                if (FAILED(hr)) {
+                    std::cerr << "Failed to release render buffer!" << std::endl;
+                    break;
+                }
+            }
+        }// Отправляем обработанные данные на вывод
+       
 
         // Освобождаем буфер захвата
         hr = pCaptureClient->ReleaseBuffer(numFramesAvailable);
@@ -235,14 +390,6 @@ int main()
             std::cerr << "Failed to release buffer!" << std::endl;
             break;
         }
-
-        // Отправляем буфер на воспроизведение
-        hr = pAudioRender->ReleaseBuffer(numFramesAvailable, 0);
-        if (FAILED(hr)) {
-            std::cerr << "Failed to release render buffer!" << std::endl;
-            break;
-        }
-        processedData.clear();
     }
     pAudioClient->Stop();
     pCaptureClient->Release();
