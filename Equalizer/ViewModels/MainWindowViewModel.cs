d91a5dc000f13203d8b0fcf7b33a098da44d7e9c
bulavko <bulavko@KGT-ASUPP-17>
2025-06-10 11:50:58 +0700

    Добавьте файлы проекта.

---
+++ Equalizer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Equalizer.Service;
+using NAudio.CoreAudioApi;
+using System.Collections.ObjectModel;
+
+namespace Equalizer.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        public ObservableCollection<MMDevice> _Devices;
+        [ObservableProperty]
+        private MMDevice _SelectedDevice;
+        private AudioCaptureProcessor _Processor;
+        public MainWindowViewModel()
+        {
+            Devices = [.. AudioCaptureProcessor.GetDevices()];
+        }
+        partial void OnSelectedDeviceChanged(MMDevice value)
+        {
+            _Processor?.Dispose();
+            _Processor = new(SelectedDevice);
+        }
+        [RelayCommand]
+        private void Play()
+        {
+            _Processor.StartCapture();
+        }
+    }
+}
---
+++ Equalizer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Equalizer.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
